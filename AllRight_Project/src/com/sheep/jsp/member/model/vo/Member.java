fdc887b9e12fe93592e04a208819a20b79442f84
SeungIn <merry@serface-merry>
2018-11-05 10:52:05 +0900

    vo 추가

--- AllRight_Project/src/com/sheep/jsp/member/model/vo/Member.java
+++ AllRight_Project/src/com/sheep/jsp/member/model/vo/Member.java
@@ -12,16 +12,13 @@ public class Member implements Serializable{
 	
 	// 필드
 	
-	private int a;
 	private int userNo;
 	private String userId;
 	private String userPwd;
 	private String userName;
 	private String email;
-	private String seloption;
-	private Date enrollDate;
-	private int point;
-	
+	private String userLeave;
+	private Date userDate;
 	
 	
 	// 생성자
@@ -31,41 +28,32 @@ public class Member implements Serializable{
 	}
 
 
-	public Member(String userId, String userPwd) {
+	public Member(String userId, String userPwd, String userName, String email) {
 		super();
+		
 		this.userId = userId;
 		this.userPwd = userPwd;
+		this.userName = userName;
+		this.email = email;
 	}
 
 
-	public Member(int userNo, String userId, String userPwd, String userName, String email, String seloption) {
+	public Member(int userNo, String userId, String userPwd, String userName, String email, String userLeave,
+			Date userDate) {
 		super();
 		this.userNo = userNo;
 		this.userId = userId;
 		this.userPwd = userPwd;
 		this.userName = userName;
 		this.email = email;
-		this.seloption = seloption;
+		this.userLeave = userLeave;
+		this.userDate = userDate;
 	}
 
-
-	public Member(int userNo, String userId, String userPwd, String userName, String email, String seloption,
-			Date enrollDate ,int point) {
-		super();
-		this.userNo = userNo;
-		this.userId = userId;
-		this.userPwd = userPwd;
-		this.userName = userName;
-		this.email = email;
-		this.seloption = seloption;
-		this.enrollDate = enrollDate;
-		this.point = point;
-	}
-
-	
 	
 	
 	// getter & setter 
+	
 
 	public int getUserNo() {
 		return userNo;
@@ -117,51 +105,100 @@ public class Member implements Serializable{
 	}
 
 
-	public String getSeloption() {
-		return seloption;
+	public String getUserLeave() {
+		return userLeave;
 	}
 
 
-	public void setSeloption(String seloption) {
-		this.seloption = seloption;
+	public void setUserLeave(String userLeave) {
+		this.userLeave = userLeave;
 	}
 
 
-	public Date getEnrollDate() {
-		return enrollDate;
+	public Date getUserDate() {
+		return userDate;
 	}
 
 
-	public void setEnrollDate(Date enrollDate) {
-		this.enrollDate = enrollDate;
+	public void setUserDate(Date userDate) {
+		this.userDate = userDate;
 	}
-	
-	public int getPoint(){
-		return point;
-	}
-	
-	public void setPoint(int point){
-		this.point = point;
-	}
-	
 
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	
-	
-
 
 	
 	// toString()
-	
+
 	@Override
 	public String toString() {
 		return "Member [userNo=" + userNo + ", userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName
-				+ ", email=" + email + ", seloption=" + seloption + ", enrollDate=" + enrollDate + ", point=" + point + "]";
+				+ ", email=" + email + ", userLeave=" + userLeave + ", userDate=" + userDate + "]";
 	}
 
+	
+	// hashCode()
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((userDate == null) ? 0 : userDate.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		result = prime * result + ((userLeave == null) ? 0 : userLeave.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		result = prime * result + userNo;
+		result = prime * result + ((userPwd == null) ? 0 : userPwd.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (userDate == null) {
+			if (other.userDate != null)
+				return false;
+		} else if (!userDate.equals(other.userDate))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		if (userLeave == null) {
+			if (other.userLeave != null)
+				return false;
+		} else if (!userLeave.equals(other.userLeave))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		if (userNo != other.userNo)
+			return false;
+		if (userPwd == null) {
+			if (other.userPwd != null)
+				return false;
+		} else if (!userPwd.equals(other.userPwd))
+			return false;
+		return true;
+	}
 	
 	
 	
