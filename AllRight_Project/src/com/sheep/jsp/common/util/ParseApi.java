8c65a2a43931f4cb12e0320968952da9b25aab04
2youngrae <sjj99009@naver.com>
2018-11-14 14:59:51 +0900

    commit

--- AllRight_Project/src/com/sheep/jsp/common/util/ParseApi.java
+++ AllRight_Project/src/com/sheep/jsp/common/util/ParseApi.java
@@ -1,14 +1,25 @@
 package com.sheep.jsp.common.util;
 
 import java.util.ArrayList;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+ 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+ 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
 
 import com.sheep.jsp.licenseinfo.model.vo.LicenseInfo;
 
@@ -52,6 +63,28 @@ public class ParseApi {
 
 			// 파싱할 tag
 			nList = doc.getElementsByTagName("item");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return nList;
+	}
+	
+	private NodeList commons(String url) {
+
+		NodeList nList = null;
+		try {
+
+			DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
+			Document doc = dBuilder.parse(url);
+
+			// root tag
+			doc.getDocumentElement().normalize();
+
+			// 파싱할 tag
+			nList = doc.getElementsByTagName("//items/item");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -125,42 +158,92 @@ public class ParseApi {
 
 	public ArrayList<LicenseInfo> getInfoList(ArrayList<LicenseInfo> list) {
 		
-		int size = list.size();
-		int i = 0;
 		
+		testGetApi();
+//		int size = list.size();
+//		int i = 0;
+//		
 		ArrayList<LicenseInfo> resultList = new ArrayList<LicenseInfo>();
-		
-		// licenseInfo :  info 업데이트전 자격증 정보 한건
-		for(LicenseInfo licenseInfo :  list) {
-			System.out.println("자격증 정보 업데이트 중"+"("+ (i+1) +"/"+ size +")");
-					
-			String url = "http://openapi.q-net.or.kr/api/service/rest/InquiryInformationTradeNTQSVC/getList?ServiceKey=Oi%2FEbWNVg5PdT0l9KErR0viwEKN9SzcsbQaeVE%2BxvL3%2FYY0FT1vmy3qVxHNj1HPH4vO0x6LdFRETO8txrEDnxQ%3D%3D&jmCd=";
-					
-			url += licenseInfo.getlNo();
-					
-			NodeList nList = common(url);
-
-
-			for (int j = 0; j < nList.getLength(); j++) {
-						
-				Node nNode = nList.item(j);
-				
-					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-						Element eElement = (Element) nNode;
-							
-						// 자격증에 info 데이터 setting
-						licenseInfo.setlInfo(getTagValues("contents", eElement));
-					}
-			}
-			
-			i++;
-			
-			resultList.add(licenseInfo);
-		}
-		
-		
+//		
+//		// licenseInfo :  info 업데이트전 자격증 정보 한건
+//		for(LicenseInfo licenseInfo :  list) {
+//			System.out.println("자격증 정보 업데이트 중"+"("+ (i+1) +"/"+ size +")");
+//					
+//			String url = "http://openapi.q-net.or.kr/api/service/rest/InquiryInformationTradeNTQSVC/getList?ServiceKey=Oi%2FEbWNVg5PdT0l9KErR0viwEKN9SzcsbQaeVE%2BxvL3%2FYY0FT1vmy3qVxHNj1HPH4vO0x6LdFRETO8txrEDnxQ%3D%3D&jmCd=";
+//					
+//			url += licenseInfo.getlNo();
+//					
+//			NodeList nList = commons(url);
+//
+//
+//			for (int j = 0; j < nList.getLength(); j++) {
+//						
+//				Node nNode = nList.item(j);
+//				
+//					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+//
+//						Element eElement = (Element) nNode;
+//							
+//						// 자격증에 info 데이터 setting
+//						licenseInfo.setlInfo(getTagValues("contents", eElement));
+//					}
+//			}
+//			
+//			i++;
+//			
+//			resultList.add(licenseInfo);
+//		}
+//		
+//		
 		return resultList;
+	}
+	
+	public void testGetApi(){
+		BufferedReader br = null;
+        //DocumentBuilderFactory 생성
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder builder;
+        Document doc = null;
+        try {
+            //OpenApi호출
+            String urlstr = "http://openapi.q-net.or.kr/api/service/rest/InquiryInformationTradeNTQSVC/getList?ServiceKey=Oi%2FEbWNVg5PdT0l9KErR0viwEKN9SzcsbQaeVE%2BxvL3%2FYY0FT1vmy3qVxHNj1HPH4vO0x6LdFRETO8txrEDnxQ%3D%3D&jmCd=";
+            urlstr += "1320";
+            URL url = new URL(urlstr);
+            HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+            
+            //응답 읽기
+            br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
+            String result = "";
+            String line;
+            while ((line = br.readLine()) != null) {
+                result = result + line.trim();// result = URL로 XML을 읽은 값
+            }
+            
+            // xml 파싱하기
+            InputSource is = new InputSource(new StringReader(result));
+            builder = factory.newDocumentBuilder();
+            doc = builder.parse(is);
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+            XPath xpath = xpathFactory.newXPath();
+            // XPathExpression expr = xpath.compile("/response/body/items/item");
+            XPathExpression expr = xpath.compile("//items/item/contents");
+            NodeList nodeList = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                NodeList child = nodeList.item(i).getChildNodes();
+                for (int j = 0; j < child.getLength(); j++) {
+                    Node node = child.item(j);
+//                    System.out.println("현재 노드 이름 : " + node.getNodeName());
+//                    System.out.println("현재 노드 타입 : " + node.getNodeType());
+                    System.out.println("현재 노드 값 : " + node.getTextContent());
+//                    System.out.println("현재 노드 네임스페이스 : " + node.getPrefix());
+//                    System.out.println("현재 노드의 다음 노드 : " + node.getNextSibling());
+                    System.out.println("");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 	}
 
 }
