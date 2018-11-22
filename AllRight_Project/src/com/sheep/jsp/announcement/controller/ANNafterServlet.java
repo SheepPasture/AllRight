030a57054f3a6f26274077cc698069ff15ee908a
Mereepe <42257703+Mereepe@users.noreply.github.com>
2018-11-22 23:36:08 +0900

    Update ANNafterServlet.java

--- AllRight_Project/src/com/sheep/jsp/announcement/controller/ANNafterServlet.java
+++ AllRight_Project/src/com/sheep/jsp/announcement/controller/ANNafterServlet.java
@@ -1,54 +1,59 @@
 package com.sheep.jsp.announcement.controller;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
+ import java.io.IOException;
+import java.util.ArrayList;
+ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.sheep.jsp.announcement.model.service.ANNService;
-import com.sheep.jsp.announcement.model.vo.Announcement;
-
-/**
- * Servlet implementation class ANNafterServlet
- */
-@WebServlet("/aAfter.ann")
-public class ANNafterServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ANNafterServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ import javax.servlet.http.HttpServletRequest;
+ import javax.servlet.http.HttpServletResponse;
+ 
+ import com.sheep.jsp.announcement.model.service.ANNService;
+ import com.sheep.jsp.announcement.model.vo.Announcement;
+ 
+ /**
+  * Servlet implementation class ANNafterServlet
+  */
+ @WebServlet("/aAfter.ann")
+ public class ANNafterServlet extends HttpServlet {
+ 	private static final long serialVersionUID = 1L;
+        
+     /**
+      * @see HttpServlet#HttpServlet()
+      */
+     public ANNafterServlet() {
+         super();
+         // TODO Auto-generated constructor stub
+     }
+ 
+ 	/**
+ 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+ 	 */
+ 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		int ano = Integer.parseInt(request.getParameter("ano"));
 		
+		ArrayList<Announcement> fAno = new ANNService().afterANN(ano);
+		
+		Announcement a = new ANNService().selectOne(fAno.get(1));
 		int fAno = new ANNService().afterANN(ano);
 		
+		System.out.println("a:" +a);
 		Announcement a = new ANNService().selectOne(fAno);
-
-		System.out.println("ANNbeforeServlet ano: "+ano);
+ 		System.out.println("ANNbeforeServlet ano: "+ano);
 		System.out.println("ANNbeforeServlet nAno: "+fAno);
 		System.out.println("ANNbeforeServlet a: "+a);
-		
-		String page = "";
-		
+ 		
+ 		String page = "";
+ 		
 		if(a != null){
 			page = "/views/announcement/ANNDetail.jsp";
 			request.setAttribute("announcement", a);
+			request.setAttribute("fAno", fAno);
 			
 		} else{
 			page="/views/common/errorPage.jsp";
 			request.setAttribute("msg", "공지사항 상세보기에 실패하였습니다. 관리자에게 문의바랍니다.");
+
 		}
 		
 		request.getRequestDispatcher(page).forward(request, response);
